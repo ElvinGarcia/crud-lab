039dbaa251f493b5139f65fb325009172c955a43
Elvin <Elvin@ElvinGarcia.com>
2022-05-16 23:38:24 -0400

    all restaurants test passing. Previous failed test where due to a miss spelling in the RestaurantContainer.js section mapDispatchToProps. Missed spelled Restaurnt instead of Restaurant same error occured in the restaurant reducer

--- src/reducers/manageRestaurant.js
+++ src/reducers/manageRestaurant.js
@@ -2,17 +2,23 @@ import cuid from 'cuid';
 export const cuidFn = cuid;
 
 export default function manageRestaurants(state = {
-  restaurants: [],}, action) {
+  restaurants: [],
+  reviews:[],
+}, action) {
 
   switch (action.type) {
-    case "ADD_RESTAURNT":
+
+    case "ADD_RESTAURANT":
       const obj = {
         text: action.text,
         id: cuidFn(),
       }
-      return {
-        ...state,restaurants:[...state.restaurants,obj]
-      }
+      return { ...state, restaurants: [...state.restaurants, obj] }
+
+    case "DELETE_RESTAURANT":
+      const restaurantList = state.restaurants.filter((restaurant) => restaurant.id !== action.id)
+      return { ...state, restaurants:restaurantList }
+
     default:
       return state;
   }
