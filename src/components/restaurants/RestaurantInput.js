0b2ef61ecabc5509fb3fdc64981e85add51edfcd
Elvin <Elvin@ElvinGarcia.com>
2022-05-16 22:15:43 -0400

    RestaurantInput test passing

--- src/components/restaurants/RestaurantInput.js
+++ src/components/restaurants/RestaurantInput.js
@@ -1,10 +1,27 @@
 import React, { Component } from 'react';
 
 class RestaurantInput extends Component {
+  state = {
+        text: ''
+  }
+
+  handleOnchange = (event) => {
+    this.setState({text:event.target.value})
+  }
+
+  handleOnSubmit = (event) => {
+    event.preventDefault();
+    this.props.addRestaurant(this.state.text)
+    this.setState({text:''})
+  }
+
   render() {
     return (
       <div>
-        Restaurant Input
+        <form onSubmit={this.handleOnSubmit}>
+          <input type="text" name="text" value={this.state.text} onChange={this.handleOnchange} />
+          <input type="submit"/>
+        </form>
       </div>
     );
   }
--- src/reducers/manageRestaurant.js
+++ src/reducers/manageRestaurant.js
@@ -1,6 +1,20 @@
 import cuid from 'cuid';
 export const cuidFn = cuid;
- 
-export default function manageRestaurants(state, action) {
+
+export default function manageRestaurants(state = {
+  restaurants: [],}, action) {
+
+  switch (action.type) {
+    case "ADD_RESTAURNT":
+      const obj = {
+        text: action.text,
+        id: cuidFn(),
+      }
+      return {
+        ...state,restaurants:[...state.restaurants,obj]
+      }
+    default:
+      return state;
+  }
 
 }
